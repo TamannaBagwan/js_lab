226a2a4d6e9823397dc71ba30f587bf72b50a322
TamannaBagwan <tamannabagwan1412@gmail.com>
2024-04-03 21:43:46 +0530

    started

---
+++ Basics/Loops.js
@@ -0,0 +1,23 @@
+
+// i++ = i+1 
+
+// for(var i=0;i<5;i++){
+//     console.log("i :", i);
+// }
+
+
+//2.while loop ==>
+// let i =0
+// while(i<5){
+//     console.log('while loop' , i);
+//     i++
+// }
+
+
+
+//3. do while loop==>
+let i = 1
+do{
+    console.log('inside do while loop',i); 
+    i++
+}while(i<10)
